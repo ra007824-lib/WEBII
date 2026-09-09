d4167750def93f7c8551c2a60bf0bef02549b767
Rafael Santos de Padua <007824@libertas.edu.br>
2026-09-09 19:46:40 -0300

    Delete JULIANO directory
    Delete JULIANO directory

--- JULIANO/Lista_Condicoes/js/ex001.js
+++
@@ -1,9 +0,0 @@
-frm = document.querySelector("form");
-
-frm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const n = Number(frm.inNumero.value);
-    
-    document.querySelector("h3").innerText = n %2==0 ? "o numero é par" : "o numero é impar";
-    
-})
--- JULIANO/variaveis/aula 04/js/ex01.js
+++
@@ -1,8 +0,0 @@
-const frm = document.querySelector('form');
-const resp = document.querySelector("h3");
-//cria um 'ouvinte' de envento, acionado quando o botão submit for clicado
-frm.addEventListener("submit",(e)=> {
-    const nome = frm.InNome.value; // obtem valor da cx de texto
-    resp.innerText = `Olá ${nome}`;
-    e.preventDefault();// evita o envio do form
-})
